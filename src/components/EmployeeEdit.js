03aff27541b32def6ac7add505e1b4582f518de3
Fahri Cavusoglu <fahric@hotmail.co.uk>
2018-05-03 17:10:08 +0100

    added confirm modal

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,9 +1,10 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
+import Communications from 'react-native-communications';
 import { connect } from 'react-redux';
 import EmployeeForm from './EmployeeForm';
 import { employeeUpdate, employeeSave } from '../actions';
-import { Card, CardItem, Button } from './common';
+import { Card, CardItem, Button, Confirm } from './common';
 
 class EmployeeEdit extends Component {
     componentWillMount() {
@@ -17,12 +18,22 @@ class EmployeeEdit extends Component {
         this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid });
     }
 
+    onTextPress() {
+        const { phone, shift } = this.props;
+        Communications.text(phone, `Your shift is ${shift}`);
+    }
+
     render() {
         return (<Card>
             <EmployeeForm />
             <CardItem>
                 <Button onPress={this.onButtonPress.bind(this)}>Save Changes</Button>
             </CardItem>
+            <CardItem>
+            <Button onPress={this.onTextPress.bind(this)}>Text Schedule</Button>
+            </CardItem>
+
+            <Confirm>Are you sure you want to delete?</Confirm>
         </Card>);
     }
 }
